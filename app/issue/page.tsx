906279e1336b9455970499761bad6621e8938cd2
mohamad8616 <mohamad.sh.a9036@gmail.com>
2025-08-21 16:46:01 +0330

    migrate model in prisma

--- app/issue/page.tsx
+++ app/issue/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+export const metadata = {
+  title: "Issue Page",
+  description: "This is the issue page",
+};
+
 export default function page() {
   return <div>page</div>;
 }
